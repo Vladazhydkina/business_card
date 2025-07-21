c294298bd11348f79abdb492d01e20be41f31cf4
Vladazhydkina <zidvlada@gmail.com>
2025-07-22 00:36:40 +0300

    updated styles and main block

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,17 +1,13 @@
-import React from "react";
-
 function Footer(){
     return(
         <div className="footer">
-            <p>
-                P.S. I know you liked the business card, <br/>
-                if you want the same one - write to me 😉
-            </p>
 
             <p className="spaced-text">
                 Add to your contacts👇
             </p>
-            <a href="https://pmkopgnvfxs5s3us.public.blob.vercel-storage.com/zhydkina_contact-mDbgjCPV75Hx9nRRbHltTPYGpAybsP.vcf" download className="contact_button">add contacts</a>
+            <a href="https://pmkopgnvfxs5s3us.public.blob.vercel-storage.com/zhydkina_contact-mDbgjCPV75Hx9nRRbHltTPYGpAybsP.vcf" download className="contact_button">
+            add contacts
+            </a>
         </div>
     )
 }
--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,27 +1,47 @@
 import React from "react";
+import MailOutlineIcon from '@mui/icons-material/MailOutline';
+import TelegramIcon from '@mui/icons-material/Telegram';
+import LinkedInIcon from '@mui/icons-material/LinkedIn';
 
-function Main(){
+function Main(props){
     return(
         <div className="container"> 
         <div className="main_container">
 
-            <div className="contact-block">
-                <a href="https://www.linkedin.com/in/vladyslava-zhydkina/" target="_blank">
-                <p>Linkedin</p>
-            </a>
-            </div>
-            <div className="contact-block">
-                <a href="https://t.me/v_zhyyy" target="_blank">
-                <p>Telegram</p>
-                </a>
-            </div>
-            <div className="contact-block">
-                <a href="mailto:v.zhy.dev@gmail.com">
-                <p>Mail</p>
-                </a>
-            </div>
-            
+            <ContactCard
+            link="https://www.linkedin.com/in/vladyslava-zhydkina/"
+            icon={LinkedInIcon}
+            label="LinkedIn"
+            />
+
+            <ContactCard
+            link="https://t.me/v_zhyyy"
+            icon={TelegramIcon}
+            label="Telegram"
+            />
+
+            <ContactCard
+            link="mailto:v.zhy.dev@gmail.com"
+            icon={MailOutlineIcon}
+            label="Mail"
+            />
         </div>
+                    <p>
+                P.S. I know you liked the business card, <br/>
+                if you want the same one - write to me 😉
+            </p>
+        </div>
+    )
+}
+
+function ContactCard(props){
+    const Icon = props.icon;
+    return(
+        <div className="contact-block">
+            <a href={props.link} target="_blank" rel="noopener noreferrer">
+            <Icon sx={{ color: "#A7A7A7" }}/>
+            <p className="contact-text">{props.label}</p>
+            </a>
         </div>
     )
 }
